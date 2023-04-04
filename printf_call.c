87828a3b207fc7148e6477948cc760dd50b2a708
MishenkovDaniil <mishenkovdaniil@mail.ru>
2023-04-04 20:22:18 +0300

    add examples of using my printf

--- printf_call.c
+++ printf_call.c
@@ -6,7 +6,7 @@ extern "C" void strt_printf (const char *format,...);
 int main ()
 {
     
-    strt_printf ("%d %d %o %c\n ", (long)-2, 15, 14, 'f');
-    
+    strt_printf ("<<<%d %d %o %c \n %d %s %x %d%%%c%b\n >>>", (long)-2, 15, 14, 'd', -1L, "love", 3802, 100, 33, 127);
+    strt_printf ("\n<<<%d%f>>>\n", 1);    
     return 0;
 }
